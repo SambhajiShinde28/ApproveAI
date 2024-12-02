70fc0ef13a7201340521ed57618759d2509dcc43
SambhajiShinde28 <sambhajisshinde800@gmail.com>
2024-12-02 23:49:51 +0530

    Changing in addmin & model file in my project.

--- approveai_backend/loan_approve_app/admin.py
+++ approveai_backend/loan_approve_app/admin.py
@@ -3,6 +3,6 @@ from .models import LoanPredictionModel
 
 # Register your models here.
 class Loan_Prediction_Admin(admin.ModelAdmin):
-    list_display = ('FullName', 'Address', 'Gender', 'Married', 'Dependence', 'Education', 'SelfEmployee', 'ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'Loan_Amount_Term', 'Credit_History', 'PropertyArea', 'RejectedPercent', 'ApprovedPercent', 'ModelPrediction', 'LoanStatus')
+    list_display = ('FullName', 'Address', 'Gender', 'Married', 'Dependence', 'Education', 'SelfEmployee', 'ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'LoanAmountTerm', 'CreditHistory', 'PropertyArea', 'RejectedPercentage', 'ApprovedPercentage', 'ModelPrediction', 'LoanStatus')
 
 admin.site.register(LoanPredictionModel,Loan_Prediction_Admin)
--- approveai_backend/loan_approve_app/views.py
+++ approveai_backend/loan_approve_app/views.py
@@ -1,6 +1,38 @@
-from django.shortcuts import render
-from django.http import HttpResponse,JsonResponse
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import LoanPredictionModel
+import json
 
-# Create your views here.
+
+@csrf_exempt
 def LoanPrediction(req):
-    return HttpResponse("All Ok")
+
+    if req.method == "POST":
+
+        data = json.load(req)
+
+        fullName=data["FullName"]
+        address=data["Address"]
+        gender=data['Gender']
+        married=data['Married']
+        dependents=data['Dependents']
+        education=data['Education']
+        selfEmployed=data['SelfEmployed']
+        applicantIncome=data['ApplicantIncome']
+        coapplicantIncome=data['CoapplicantIncome']
+        loanAmount=data['LoanAmount']
+        loanAmountTerm=data['LoanAmountTerm']
+        creditHistory=data['CreditHistory']
+        propertyArea=data['PropertyArea']
+
+        # print(f"Model PRediction : {fullName} {address} {gender} {married} {dependents} {education} {selfEmployed} {applicantIncome} {coapplicantIncome} {loanAmount} {loanAmountTerm} {creditHistory} {propertyArea}")
+
+        if fullName != "" and address != "" and gender != "" and married != "" and dependents != "" and education != "" and selfEmployed != "" and applicantIncome != "" and coapplicantIncome != "" and loanAmount != "" and loanAmountTerm != "" and creditHistory != "" and propertyArea != "":
+            pass
+
+        else:
+            return JsonResponse({"message":"fields empty"})
+
+        return JsonResponse({"message":"This is post request"})
+    else:
+        return JsonResponse({"message":"False"})
