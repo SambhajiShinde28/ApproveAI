ff48d21163036a8483deb542842f4f6f707ad337
SambhajiShinde28 <sambhajisshinde800@gmail.com>
2024-12-04 20:09:08 +0530

    Uploaded Django with Mysql connection.

--- approveai_backend/approveai_backend/settings.py
+++ approveai_backend/approveai_backend/settings.py
@@ -75,11 +75,16 @@ WSGI_APPLICATION = 'approveai_backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'approveai_database',
+		'USER': 'root',
+		'PASSWORD': 'Sam#26',
+		'HOST':'localhost',
+		'PORT':'3306',
+	}
 }
+
 
 
 # Password validation
